56e34b19525918db8ad3f3b28c70715b42bd963e
alberto-2-garcia <alberto.2.garcia@atos.net>
2022-04-11 16:27:48 -0500

    Setup of gh pages

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,16 +6,16 @@ function NavbarComponent() {
   return (
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container>
-        <Link href='/' passHref>
+        <Link href='/' as={process.env.BACKEND_URL + '/'} passHref>
           <Navbar.Brand>Alberto's Portfolio</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link href='/blog' passHref>
+            <Link href='/blog' as={process.env.BACKEND_URL + '/blog'} passHref>
               <Nav.Link>Blog</Nav.Link>
             </Link>
-            <Link href='/github' passHref>
+            <Link href='/github' as={process.env.BACKEND_URL + '/github'} passHref>
               <Nav.Link>Github</Nav.Link>
             </Link>
           </Nav>
